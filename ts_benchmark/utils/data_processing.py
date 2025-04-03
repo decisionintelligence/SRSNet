4938491b72064f036ad04c9f79f637d8c2ad1055
ccloud0525 <1793410861@qq.com>
2025-04-03 14:26:31 +0800

    feat: Update the new SRSNet with chosable MLP/Linear head

--- ts_benchmark/utils/data_processing.py
+++ ts_benchmark/utils/data_processing.py
@@ -14,3 +14,13 @@ def split_before(data: pd.DataFrame, index: int) -> Tuple[pd.DataFrame, pd.DataF
     :return: Split the first and second half of the data.
     """
     return data.iloc[:index, :], data.iloc[index:, :]
+
+def split_time(data: pd.DataFrame, index: int) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Split time series data into two parts at the specified index.
+
+    :param data: Time series data to be segmented.
+    :param index: Split index position.
+    :return: Split the first and second half of the data.
+    """
+    return data.iloc[:index, :], data.iloc[index:, :]
