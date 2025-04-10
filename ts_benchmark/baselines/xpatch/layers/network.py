7b67aa9199aa9111fdbfc4a75bd0709868758819
ccloud0525 <1793410861@qq.com>
2025-04-10 10:02:31 +0800

    feat: Add PatchMLP

--- ts_benchmark/baselines/xpatch/layers/network.py
+++ ts_benchmark/baselines/xpatch/layers/network.py
@@ -1,12 +1,16 @@
 import torch
 from torch import nn
+from ..layers.SRS import SRS
+import math
 
 class Network(nn.Module):
-    def __init__(self, seq_len, pred_len, patch_len, stride, padding_patch):
+    def __init__(self, seq_len, pred_len, patch_len, stride, padding_patch, srs_dropout, srs_hidden_size, srs_alpha,
+                 srs_pos):
         super(Network, self).__init__()
 
         # Parameters
         self.pred_len = pred_len
+        self.seq_len = seq_len
 
         # Non-linear Stream
         # Patching
@@ -14,16 +18,18 @@ class Network(nn.Module):
         self.stride = stride
         self.padding_patch = padding_patch
         self.dim = patch_len * patch_len
-        self.patch_num = (seq_len - patch_len)//stride + 1
-        if padding_patch == 'end': # can be modified to general case
-            self.padding_patch_layer = nn.ReplicationPad1d((0, stride)) 
-            self.patch_num += 1
+        self.patch_num = math.ceil((self.seq_len - self.patch_len) / self.stride) + 1
+        # if padding_patch == 'end':  # can be modified to general case
+        #     self.padding_patch_layer = nn.ReplicationPad1d((0, stride))
+        #     self.patch_num += 1
 
         # Patch Embedding
-        self.fc1 = nn.Linear(patch_len, self.dim)
+        # self.fc1 = nn.Linear(patch_len, self.dim)
+        self.fc1 = SRS(self.dim, self.patch_len, self.stride, self.seq_len, srs_dropout, srs_hidden_size, srs_alpha, srs_pos)
+
         self.gelu1 = nn.GELU()
         self.bn1 = nn.BatchNorm1d(self.patch_num)
-        
+
         # CNN Depthwise
         self.conv1 = nn.Conv1d(self.patch_num, self.patch_num,
                                patch_len, patch_len, groups=self.patch_num)
@@ -63,26 +69,26 @@ class Network(nn.Module):
         # x: [Batch, Input, Channel]
         # s - seasonality
         # t - trend
-        
-        s = s.permute(0,2,1) # to [Batch, Channel, Input]
-        t = t.permute(0,2,1) # to [Batch, Channel, Input]
-        
+
+        s = s.permute(0, 2, 1)  # to [Batch, Channel, Input]
+        t = t.permute(0, 2, 1)  # to [Batch, Channel, Input]
+
         # Channel split for channel independence
-        B = s.shape[0] # Batch size
-        C = s.shape[1] # Channel size
-        I = s.shape[2] # Input size
-        s = torch.reshape(s, (B*C, I)) # [Batch and Channel, Input]
-        t = torch.reshape(t, (B*C, I)) # [Batch and Channel, Input]
+        B = s.shape[0]  # Batch size
+        C = s.shape[1]  # Channel size
+        I = s.shape[2]  # Input size
+        # s = torch.reshape(s, (B * C, I))  # [Batch and Channel, Input]
+        t = torch.reshape(t, (B * C, I))  # [Batch and Channel, Input]
 
         # Non-linear Stream
         # Patching
-        if self.padding_patch == 'end':
-            s = self.padding_patch_layer(s)
-        s = s.unfold(dimension=-1, size=self.patch_len, step=self.stride)
+        # if self.padding_patch == 'end':
+        #     s = self.padding_patch_layer(s)
+        # s = s.unfold(dimension=-1, size=self.patch_len, step=self.stride)
         # s: [Batch and Channel, Patch_num, Patch_len]
-        
+
         # Patch Embedding
-        s = self.fc1(s)
+        s, _ = self.fc1(s)
         s = self.gelu1(s)
         s = self.bn1(s)
 
@@ -125,8 +131,8 @@ class Network(nn.Module):
         x = self.fc8(x)
 
         # Channel concatination
-        x = torch.reshape(x, (B, C, self.pred_len)) # [Batch, Channel, Output]
+        x = torch.reshape(x, (B, C, self.pred_len))  # [Batch, Channel, Output]
 
-        x = x.permute(0,2,1) # to [Batch, Output, Channel]
+        x = x.permute(0, 2, 1)  # to [Batch, Output, Channel]
 
         return x
--- ts_benchmark/baselines/xpatch/models/xPatch.py
+++ ts_benchmark/baselines/xpatch/models/xPatch.py
@@ -8,14 +8,15 @@ from ..layers.network import Network
 # from layers.network_cnn import NetworkCNN # For ablation study with CNN-only stream
 from ..layers.revin import RevIN
 
+
 class xPatchModel(nn.Module):
     def __init__(self, configs):
         super(xPatchModel, self).__init__()
 
         # Parameters
-        seq_len = configs.seq_len   # lookback window L
-        pred_len = configs.pred_len # prediction length (96, 192, 336, 720)
-        c_in = configs.enc_in       # input channels
+        seq_len = configs.seq_len  # lookback window L
+        pred_len = configs.pred_len  # prediction length (96, 192, 336, 720)
+        c_in = configs.enc_in  # input channels
 
         # Patching
         patch_len = configs.patch_len
@@ -24,15 +25,16 @@ class xPatchModel(nn.Module):
 
         # Normalization
         self.revin = configs.revin
-        self.revin_layer = RevIN(c_in,affine=True,subtract_last=False)
+        self.revin_layer = RevIN(c_in, affine=True, subtract_last=False)
 
         # Moving Average
         self.ma_type = configs.ma_type
-        alpha = configs.alpha       # smoothing factor for EMA (Exponential Moving Average)
-        beta = configs.beta         # smoothing factor for DEMA (Double Exponential Moving Average)
+        alpha = configs.alpha  # smoothing factor for EMA (Exponential Moving Average)
+        beta = configs.beta  # smoothing factor for DEMA (Double Exponential Moving Average)
 
         self.decomp = DECOMP(self.ma_type, alpha, beta)
-        self.net = Network(seq_len, pred_len, patch_len, stride, padding_patch)
+        self.net = Network(seq_len, pred_len, patch_len, stride, padding_patch, configs.srs_dropout,
+                           configs.srs_hidden_size, configs.srs_alpha, configs.srs_pos)
         # self.net_mlp = NetworkMLP(seq_len, pred_len) # For ablation study with MLP-only stream
         # self.net_cnn = NetworkCNN(seq_len, pred_len, patch_len, stride, padding_patch) # For ablation study with CNN-only stream
 
@@ -43,7 +45,7 @@ class xPatchModel(nn.Module):
         if self.revin:
             x = self.revin_layer(x, 'norm')
 
-        if self.ma_type == 'reg':   # If no decomposition, directly pass the input to the network
+        if self.ma_type == 'reg':  # If no decomposition, directly pass the input to the network
             x = self.net(x, x)
             # x = self.net_mlp(x) # For ablation study with MLP-only stream
             # x = self.net_cnn(x) # For ablation study with CNN-only stream
